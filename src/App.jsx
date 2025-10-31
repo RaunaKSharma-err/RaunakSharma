fca043620b29990ce5d34b570d9e2791e0df84ce
RaunaKSharma-err <progritik@gmail.com>
2025-10-31 20:20:42 +0545

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import Navbar from "./sections/Navbaravbar";
+import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
